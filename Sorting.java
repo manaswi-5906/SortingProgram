9d4582eca03587468d28a6234e2456a9721f0721
manaswi-5906 <manaswi5906@gmail.com>
2026-03-15 14:25:27 +0530

    Version 3:Added user input for sorting program
    Modified the sorting program to accept user input for the number of elements and the elements themselves, replacing the hardcoded array.

--- Sorting.java
+++ Sorting.java
@@ -1,19 +1,27 @@
 //Java Program to sort an array of integer
+import java.util.Scanner;
 class sorting{
     public static void main(String[] args){
-         int arr[] = {5,2,8,1};//Array elements
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                 if(arr[i]>arr[j]){
-                       int temp;
-                       arr[i]=arr[j];
-                       arr[j]=temp;
-                 }
-            }
+       Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number of elements:");
+        int n=sc.nextInt();
+        int temp;
+
+     System.out.println("Enter elements:");
+     for(int i=0;i<n;i++){
+           arr[i]=sc.nextInt();
+     }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+             if(arr[i] > arr[j]){
+                  temp=arr[i];
+                   arr[i]=arr[j];
+                    arr[j]=temp;
+             }
         }
-    //Display Sorting array
-        System.out.println("Sorting array:");
-        for(int i=0;i<arr.length;i++){
+    }
+    System.out.println("Sorted array:");
+        for(int i=0;i<n;i++){
             System.out.print(arr[i] + " ");
         }
     }
